2792dc069a2cbb1551ef0dff45b46b16afae9ba2
Richard Gibert <richard@gibert.ca>
2017-12-29 11:38:06 -0500

    add sandbox inventory json input for testing inventory script, fixes for collecting json data from Ambari

--- ambari/ansible-inventory.py
+++ ambari/ansible-inventory.py
@@ -14,20 +14,21 @@ class AmbariInventory(object):
 
     def __init__(self):
         # TODO: load settings from a config file
-        self.cluster_name = ''
-        self.ambari_host = ''
-        self.uri = ''
-        self.ambari_user = ''
-        self.ambari_pass = ''
+        self.cluster_name = 'Sandbox'
+        self.ambari_host = 'localhost'
+        self.uri = 'http://localhost:8080'
+        self.ambari_user = 'admin'
+        self.ambari_pass = 'admin'
 
-        args = process_args()
-
-        service_list = get_service_list()
+        args = self.process_args()
+        service_list = self.get_service_list()
+        print service_list
 
         # Called with `--list`.
         if args.list:
-            json.dumps(generate_ambari_inventory(service_list))
+            json.dumps(self.generate_ambari_inventory(service_list))
         elif args.host:
+            json.dumps(self.generate_ambari_inventory(service_list))
 
 
     def process_args(self):
@@ -38,31 +39,35 @@ class AmbariInventory(object):
 
     # Handle API calls to Ambari
     def ambari_get(self, path):
-        return requests.get(uri + '/api/v1/clusters/' + self.cluster_name + path, auth=(ambari_user, ambari_pass), verify=False)
+        return requests.get(self.uri + '/api/v1/clusters/' + self.cluster_name + path, auth=(self.ambari_user, self.ambari_pass), verify=False)
 
     # hosts/services/components from Ambari
     def get_service_list(self):
         services = {
             'ambari': {
-                self.ambari_host
+                'server': [self.ambari_host]
             }
         }
 
-        result = ambari_get('/services')
+        result = self.ambari_get('/services')
+
         if result.status_code == requests.codes.ok:
             for item in json.loads(result.text)['items']:
                 services[item['ServiceInfo']['service_name']] = {}
 
             for service in services:
-                result = ambari_get('/services/' + service + '/components')
-                if result.status_code == requests.codes.ok:
-                    for item in json.loads(result)['items']:
-                        component_name = item['ServiceComponentInfo']['component_name']
+                result = self.ambari_get('/services/' + service + '/components')
 
+                if result.status_code == requests.codes.ok:
+                    for item in json.loads(result.text)['items']:
+                        component_name = item['ServiceComponentInfo']['component_name']
                         services[service][component_name] = []
 
-                        for host in ambari_get('/services/' + service + '/components/' + component_name + '?fields=host_components/HostRoles/host_name')['host_components']:
-                            services[service][component_name].append(host['HostRoles']['host_name'])
+                        result = self.ambari_get('/services/' + service + '/components/' + component_name + '?fields=host_components/HostRoles/host_name')
+
+                        if result.status_code == requests.codes.ok:
+                            for host in json.loads(result.text)['host_components']:
+                                services[service][component_name].append(host['HostRoles']['host_name'])
 
         return services
 
@@ -72,15 +77,15 @@ class AmbariInventory(object):
             'all': {
                 'hosts': []
             },
-            cluster: {
+            self.cluster_name: {
                 'children': []
             },
             'hadoop': {
-                'children': [cluster]
+                'children': [self.cluster_name]
             },
             'ambari-agent': {
                 'children': ['hadoop']
-            }
+            },
             '_meta': {
                 'hostvars': {}
             }
@@ -89,35 +94,35 @@ class AmbariInventory(object):
         # Loop over services
         for service_k, service_v in services.iteritems():
             # Add the service group as a child of the cluster group
-            if cluster + '-' + service_k not in inventory[cluster]['children']:
-                inventory[cluster]['children'].append(cluster + '-' + service_k)
+            if self.cluster_name + '-' + service_k not in inventory[self.cluster_name]['children']:
+                inventory[self.cluster_name]['children'].append(self.cluster_name + '-' + service_k)
 
             # Create a group for the service which will have all the service components as children
-            inventory[cluster + '-' + service_k] = {
-                'hosts' = [],
-                'vars' = {}
+            inventory[self.cluster_name + '-' + service_k] = {
+                'children': [],
+                'vars': {}
             }
 
             inventory[service_k] = {
-                'children': [cluster + '-' + service_k]
+                'children': [self.cluster_name + '-' + service_k]
             }
 
             # Loop over components
             for component_k, component_v in service_v.iteritems():
-                if cluster + '-' + service_k + '-' + component_k not in inventory[cluster + '-' + service_k]['children']:
-                    inventory[cluster + '-' + service_k]['children'].append(cluster + '-' + service_k + '-' + component_k)
+                if self.cluster_name + '-' + service_k + '-' + component_k not in inventory[self.cluster_name + '-' + service_k]['children']:
+                    inventory[self.cluster_name + '-' + service_k]['children'].append(self.cluster_name + '-' + service_k + '-' + component_k)
 
-                inventory[cluster + '-' + service_k + '-' + component_k] = {
-                    'hosts' = [],
-                    'vars' = {}
+                inventory[self.cluster_name + '-' + service_k + '-' + component_k] = {
+                    'hosts': [],
+                    'vars': {}
                 }
 
                 inventory[service_k + '-' + component_k] = {
-                    'children': [cluster + '-' + service_k + '-' + component_k]
+                    'children': [self.cluster_name + '-' + service_k + '-' + component_k]
                 }
 
                 for host in component_v:
-                    inventory[cluster + '-' + service_k + '-' + component_k]['hosts'].append(host)
+                    inventory[self.cluster_name + '-' + service_k + '-' + component_k]['hosts'].append(host)
 
                     if host not in inventory['all']['hosts']:
                         inventory['all']['hosts'].append(host)
